8943548d303fa4ff0b87736204f627736e450c61
DiellzaElshani <diellzaelshanni@gmail.com>
2022-01-30 22:58:48 +0100

    RDF classes with an owl property
    owl classes connected with an owl property

--- Tutorials/HelloWorld.cs
+++ Tutorials/HelloWorld.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using VDS.RDF;
 using VDS.RDF.Writing;
 using VDS.RDF.Parsing;
+using VDS.RDF.Ontology;
+
 
 public class HelloWorld
 {
@@ -10,26 +12,85 @@ public class HelloWorld
     {
         Console.WriteLine(Environment.CurrentDirectory);
 
-        Graph g = new Graph();
+        var g = new Graph();
 
-        IUriNode dotNetRDF = g.CreateUriNode(new Uri("http://www.dotnetrdf.org"));
-        IUriNode says = g.CreateUriNode(new Uri("http://example.org/says"));
-        ILiteralNode helloWorld = g.CreateLiteralNode("Hello World");
-        ILiteralNode bonjourMonde = g.CreateLiteralNode("Bonjour tout le Monde", "fr");
+        // Add namespaces (RDF and RDFS are already declared)
+        g.NamespaceMap.AddNamespace("owl", UriFactory.Create("http://www.w3.org/2002/07/owl#"));
 
-        g.Assert(new Triple(dotNetRDF, says, helloWorld));
-        g.Assert(new Triple(dotNetRDF, says, bonjourMonde));
+        // Create nodes (this could be done inline in the Assert code if you prefer
 
-        foreach (Triple t in g.Triples)
-        {
-            Console.WriteLine(t.ToString());
-        }
+        var a = g.CreateUriNode("rdf:type");
+        var owlClass = g.CreateUriNode("owl:Class");
+        var owlObjectProperty = g.CreateUriNode("owl:ObjectProperty");
+        var rdfsLabel = g.CreateUriNode("rdfs:label");
+        var rdfsDomain = g.CreateUriNode("rdfs:domain");
+        var rdfsRange = g.CreateUriNode("rdfs:range");
+        
+            
+        // Lion
+        var lion = g.CreateUriNode(UriFactory.Create("http://my.taxonomies.com/myModel/Lion"));
+        g.Assert(lion, a, owlClass);
+        g.Assert(lion, rdfsLabel, g.CreateLiteralNode("Lion", "en"));
 
-        NTriplesWriter ntwriter = new NTriplesWriter();
-        ntwriter.Save(g, "HelloWorld.nt");
+        // Animal
+        var animals = g.CreateUriNode(UriFactory.Create("http://my.taxonomies.com/myModel/Animals"));
+        g.Assert(animals, a, owlClass);
+        g.Assert(animals, rdfsLabel, g.CreateLiteralNode("Animals", "en"));
+
+        // Relation
+        var isPartOf = g.CreateUriNode("rdf:type");
+        g.Assert(isPartOf, a, owlObjectProperty);
+        g.Assert(isPartOf, rdfsLabel, g.CreateLiteralNode("isPartOf", "en"));
+
+        g.Assert(isPartOf, rdfsDomain, lion);
+        g.Assert(isPartOf, rdfsRange, animals);
+
+      // g.Assert(lion, isPartOf, animals);
+
+
+      //  var Test = g.CreateBlankNode();
+      //  var String = g.CreateVariableNode("String");
+         
+      //  g.Assert(String, String, String);
+       // g.Assert(Test, Test, Test);
+
+
+
+
+
+
+        //var BHoM = g.CreateUriNode(UriFactory.Create("https://bhom.xyz/"));
+        // g.Assert(BHoM, a, owlClass);
+        //g.Assert(BHoM, rdfsLabel, g.CreateLiteralNode("BHoM", "en"));
+
+
+        // var BHoM_Architecture_oM_Elements_Room = g.CreateUriNode(UriFactory.Create("https://github.com/BHoM/BHoM/blob/main/Architecture_oM/Elements/Room.cs"));
+        // g.Assert(BHoM_Architecture_oM_Elements_Room, a, owlClass);
+        // g.Assert(BHoM_Architecture_oM_Elements_Room, rdfsLabel, g.CreateLiteralNode("BHoM_Architecture_oM_Elements_Room", "en"));
+
+
+        //   g.Assert(BHoM_Architecture_oM_Elements_Room, isPartOf, BHoM);
+
+
+
+
+
+
+
+
+        // var o = new OntologyGraph();
+        //  var lion1 = o.CreateOntologyClass(UriFactory.Create("http://my.taxonomies.com/myModel/Lion"));
+        // lion1.AddType(UriFactory.Create(OntologyHelper.OwlClass));
+        /// lion1.AddLabel("Lion", "en");
+        //  var animals1 = o.CreateOntologyClass(UriFactory.Create("http://my.taxonomies.com/myModel/Animals"));
+        //   lion1.AddSuperClass(animals1);
+        // var isPart = o.CreateOntologyClass(UriFactory.Create("http://my.taxonomies.com/myModel"));
+        // isPart.AddType(UriFactory.Create(OntologyHelper.OwlObjectProperty));
+
 
         RdfXmlWriter rdfxmlwriter = new RdfXmlWriter();
-        rdfxmlwriter.Save(g, "HelloWorld.rdf");
+        rdfxmlwriter.Save(g, "LionAnimal.rdf");
+        //rdfxmlwriter.Save(o, "ontol.rdf");
     }
 }
 
